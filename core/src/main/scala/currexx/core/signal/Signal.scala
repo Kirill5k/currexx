0e32ff1839d1551a3ef77cbcda21793c222231bb
kirill5k <immotional@aol.com>
2022-04-02 10:47:29 +0100

    Make CurrencyPair from CurrencyPairString

--- core/src/main/scala/currexx/core/signal/Signal.scala
+++ core/src/main/scala/currexx/core/signal/Signal.scala
@@ -1,8 +1,14 @@
 package currexx.core.signal
 
-import squants.market.Currency
+import currexx.core.common.validations.CurrencyPairString
+import squants.market.{Currency, defaultMoneyContext}
 
 final case class CurrencyPair(base: Currency, quote: Currency)
+object CurrencyPair:
+  def from(cp: CurrencyPairString): CurrencyPair = {
+    val Array(base, quote) = cp.value.split("/")
+    CurrencyPair(defaultMoneyContext.currencyMap(base), defaultMoneyContext.currencyMap(quote))
+  }
 
 sealed trait Signal:
   def currencyPair: CurrencyPair
