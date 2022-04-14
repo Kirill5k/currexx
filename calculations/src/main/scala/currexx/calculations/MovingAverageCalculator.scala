ff6c419850120b90bb46650dd601a9962255d485
kirill5k <immotional@aol.com>
2022-04-14 19:54:14 +0100

    Refactor ema calc to retun all values

--- calculations/src/main/scala/currexx/calculations/MovingAverageCalculator.scala
+++ calculations/src/main/scala/currexx/calculations/MovingAverageCalculator.scala
@@ -1,17 +1,22 @@
 package currexx.calculations
 
+import scala.annotation.tailrec
+import scala.collection.mutable.ListBuffer
+
 object MovingAverageCalculator {
 
-  private val EmaSmoothing: Double = 2.0D
+  private val EmaSmoothing: BigDecimal = BigDecimal(2.0D)
 
-  def sma(values: List[BigDecimal]): BigDecimal =
-    values.sum / values.size
-
-  def ema(values: List[Double], n: Int): Double =
+  def ema(values: List[BigDecimal], n: Int): List[BigDecimal] = {
     val k = EmaSmoothing / (1 + n)
-    def calc(prices: List[Double]): Double =
-      if (prices.size == 1) prices.head
-      else prices.head * k + calc(prices.tail) * (1-k)
-    calc(values)
-
+    @tailrec
+    def calc(remainingValues: List[BigDecimal], emas: List[BigDecimal]): List[BigDecimal] =
+      if (remainingValues.isEmpty) emas
+      else {
+        val ema = remainingValues.head * k + emas.head * (1-k)
+        calc(remainingValues.tail, ema :: emas)
+      }
+    val allValues = values.reverse
+    calc(allValues.tail, allValues.head :: Nil)
+  }
 }
