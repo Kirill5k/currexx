5bdbc3e337d3c6c24b4f6ab5925b5216ffe841ab
kirill5k <immotional@aol.com>
2022-04-17 15:53:39 +0100

    Signal service tests

--- core/src/test/scala/currexx/core/signal/SignalServiceSpec.scala
+++ core/src/test/scala/currexx/core/signal/SignalServiceSpec.scala
@@ -4,8 +4,9 @@ import cats.effect.IO
 import cats.effect.unsafe.implicits.global
 import currexx.core.CatsSpec
 import currexx.domain.user.UserId
+import currexx.domain.market.{Condition, Indicator}
 import currexx.core.common.action.{Action, ActionDispatcher}
-import currexx.core.fixtures.{Signals, Users}
+import currexx.core.fixtures.{Markets, Signals, Users}
 import currexx.core.signal.db.SignalRepository
 
 class SignalServiceSpec extends CatsSpec {
@@ -37,13 +38,67 @@ class SignalServiceSpec extends CatsSpec {
 
         val result = for
           svc <- SignalService.make[IO](repo, disp)
-          res   <- svc.getAll(Users.uid)
+          res <- svc.getAll(Users.uid)
         yield res
 
         result.unsafeToFuture().map { res =>
           verifyNoInteractions(disp)
           verify(repo).getAll(Users.uid)
           res mustBe List(Signals.macd)
+        }
+      }
+    }
+
+    "processMarketData" should {
+      "not do anything when there are no changes in market data since last point" in {
+        val (repo, disp) = mocks
+
+        val result = for
+          svc <- SignalService.make[IO](repo, disp)
+          res <- svc.processMarketData(Users.uid, Markets.timeSeriesData.copy(prices = Markets.priceRanges))
+        yield res
+
+        result.unsafeToFuture().map { res =>
+          verifyNoInteractions(disp, repo)
+          res mustBe ()
+        }
+      }
+
+      "detect MACD line crossing up" in {
+        val (repo, disp) = mocks
+        when(repo.save(any[Signal])).thenReturn(IO.unit)
+        when(disp.dispatch(any[Action])).thenReturn(IO.unit)
+
+        val timeSeriesData = Markets.timeSeriesData.copy(prices = Markets.priceRanges.drop(2))
+        val result = for
+          svc <- SignalService.make[IO](repo, disp)
+          res <- svc.processMarketData(Users.uid, timeSeriesData)
+        yield res
+
+        result.unsafeToFuture().map { res =>
+          val expectedSignal = Signal(Users.uid, Markets.gbpeur, Indicator.MACD, Condition.CrossingUp(), timeSeriesData.prices.head.time)
+          verify(repo).save(expectedSignal)
+          verify(disp).dispatch(Action.SignalSubmitted(expectedSignal))
+          res mustBe ()
+        }
+      }
+
+      "detect MACD line crossing down" in {
+        val (repo, disp) = mocks
+        when(repo.save(any[Signal])).thenReturn(IO.unit)
+        when(disp.dispatch(any[Action])).thenReturn(IO.unit)
+
+        val timeSeriesData = Markets.timeSeriesData.copy(prices = Markets.priceRanges.drop(5))
+        val result = for
+          svc <- SignalService.make[IO](repo, disp)
+          res <- svc.processMarketData(Users.uid, timeSeriesData)
+        yield res
+
+        result.unsafeToFuture().map { res =>
+          val expectedSignal = Signal(Users.uid, Markets.gbpeur, Indicator.MACD, Condition.CrossingDown(), timeSeriesData.prices.head.time)
+          verify(repo).save(expectedSignal)
+          verify(disp).dispatch(Action.SignalSubmitted(expectedSignal))
+          res mustBe ()
         }
       }
     }
