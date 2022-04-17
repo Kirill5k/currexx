c80becb8d6f052cbd81dbc9997b42ce4424f3fb8
kirill5k <immotional@aol.com>
2022-04-17 14:43:10 +0100

    Use array in SMA calc

--- calculations/src/main/scala/currexx/calculations/MovingAverageCalculator.scala
+++ calculations/src/main/scala/currexx/calculations/MovingAverageCalculator.scala
@@ -5,7 +5,7 @@ import scala.collection.mutable.Queue
 
 object MovingAverageCalculator {
 
-  val EmaSmoothing: BigDecimal = BigDecimal(2.0D)
+  val EmaSmoothing: BigDecimal = BigDecimal(2.0d)
 
   private def emaAsArray(values: List[BigDecimal], n: Int, smoothing: BigDecimal = EmaSmoothing): Array[BigDecimal] = {
     val k = smoothing / (1 + n)
@@ -13,12 +13,14 @@ object MovingAverageCalculator {
     def calc(remainingValues: List[BigDecimal], emas: Array[BigDecimal], i: Int): Array[BigDecimal] =
       if (remainingValues.isEmpty) emas
       else {
-        val ema = remainingValues.head * k + emas(i) * (1-k)
-        emas.update(i-1, ema)
-        calc(remainingValues.tail, emas, i-1)
+        val ema = remainingValues.head * k + emas(i) * (1 - k)
+        emas.update(i - 1, ema)
+        calc(remainingValues.tail, emas, i - 1)
       }
     val allValues = values.reverse
-    calc(allValues.tail, Array.fill(allValues.size)(allValues.head), allValues.size - 1)
+    val emas = Array.ofDim[BigDecimal](allValues.size)
+    emas(allValues.size-1) = allValues.head
+    calc(allValues.tail, emas, allValues.size - 1)
   }
 
   def ema(values: List[BigDecimal], n: Int, smoothing: BigDecimal = EmaSmoothing): List[BigDecimal] =
@@ -26,26 +28,34 @@ object MovingAverageCalculator {
 
   def sma(values: List[BigDecimal], n: Int): List[BigDecimal] = {
     @tailrec
-    def calc(smas: List[BigDecimal], queue: Queue[BigDecimal], remaining: List[BigDecimal]): List[BigDecimal] =
-      if (remaining.isEmpty) smas
-      else if (queue.size < n) calc(smas, queue.addOne(remaining.head), remaining.tail)
+    def calc(smas: Array[BigDecimal], queue: Queue[BigDecimal], remaining: List[BigDecimal], i: Int): List[BigDecimal] =
+      if (remaining.isEmpty) smas.drop(i).toList
+      else if (queue.size < n) calc(smas, queue.addOne(remaining.head), remaining.tail, i)
       else {
         val updatedQueue = queue.drop(1).addOne(remaining.head)
-        val sma = updatedQueue.sum / n
-        calc(sma :: smas, updatedQueue, remaining.tail)
+        smas(i-1) = updatedQueue.sum / n
+        calc(smas, updatedQueue, remaining.tail, i - 1)
       }
-    calc(List.empty, Queue.empty, values.reverse)
+    val smas = Array.ofDim[BigDecimal](values.size)
+    calc(smas, Queue.empty, values.reverse, values.size)
   }
 
   def macd(values: List[BigDecimal], fastLength: Int = 12, slowLength: Int = 26): List[BigDecimal] = {
     val fastMa = emaAsArray(values, fastLength)
     val slowMa = emaAsArray(values, slowLength)
-    val macd = Array.ofDim[BigDecimal](values.size)
-    var i = 0
+    val macd   = Array.ofDim[BigDecimal](values.size)
+    var i      = 0
     while (i < values.size) {
       macd(i) = fastMa(i) - slowMa(i)
       i += 1
     }
     macd.toList
   }
+
+  def macdWithSignal(
+      values: List[BigDecimal],
+      fastLength: Int = 12,
+      slowLength: Int = 26,
+      signalSmoothing: Int = 9
+  ): (List[BigDecimal], List[BigDecimal]) = ???
 }
