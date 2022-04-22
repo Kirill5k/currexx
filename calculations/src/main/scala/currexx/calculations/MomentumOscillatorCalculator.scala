1f5968ec7cc32c4d1e3e2efae4ab804aefc7a497
kirill5k <immotional@aol.com>
2022-04-22 17:04:34 +0100

    Refactor RSI calc

--- calculations/src/main/scala/currexx/calculations/MomentumOscillatorCalculator.scala
+++ calculations/src/main/scala/currexx/calculations/MomentumOscillatorCalculator.scala
@@ -1,40 +1,31 @@
 package currexx.calculations
 
+import scala.annotation.tailrec
+
 object MomentumOscillatorCalculator {
 
-  val zero = BigDecimal(0)
+  private val zero = BigDecimal(0)
 
   def rsi(values: List[BigDecimal], length: Int): List[BigDecimal] = {
-    def calc(
-        remainingValues: List[BigDecimal],
-        prevValue: BigDecimal,
-        rsis: Array[BigDecimal],
-        i: Int,
-        gain: BigDecimal,
-        loss: BigDecimal
-    ): Array[BigDecimal] =
+    val rsis = Array.ofDim[BigDecimal](values.size)
+    @tailrec
+    def calc(remainingValues: List[BigDecimal], prevValue: BigDecimal, i: Int, gain: BigDecimal, loss: BigDecimal): Array[BigDecimal] =
       if (remainingValues.isEmpty) rsis
       else {
         val diff     = remainingValues.head - prevValue
         val currGain = diff.max(zero)
         val currLoss = diff.min(zero).abs
-        if (i < length) calc(remainingValues.tail, remainingValues.head, rsis, i + 1, gain + currGain, loss + currLoss)
-        else if (i == length) {
-          val avgGain = (gain + currLoss) / length
-          val avgLoss = (loss + currLoss) / length
-          val rsi = 100 - (100/(1 + avgGain / avgLoss))
-          rsis(i) = rsi
-          calc(remainingValues.tail, remainingValues.head, rsis, i+1, avgGain, avgLoss)
-        } else {
-          val avgGain = (gain * (length-1) + currGain) / length
-          val avgLoss = (loss * (length-1) + currLoss) / length
-          val rsi = 100 - (100/(1 + avgGain / avgLoss))
-          rsis(i) = rsi
-          calc(remainingValues.tail, remainingValues.head, rsis, i+1, avgGain, avgLoss)
+        if (i < length) calc(remainingValues.tail, remainingValues.head, i + 1, gain + currGain, loss + currLoss)
+        else {
+          val (avgGain, avgLoss) =
+            if (i == length) ((gain + currLoss) / length, (loss + currLoss) / length)
+            else ((gain * (length - 1) + currGain) / length, (loss * (length - 1) + currLoss) / length)
+          val rsi = 100 - (100 / (1 + avgGain / avgLoss))
+          rsis(values.length - i - 1) = rsi
+          calc(remainingValues.tail, remainingValues.head, i + 1, avgGain, avgLoss)
         }
       }
     val allValues = values.reverse
-    val rsis      = Array.ofDim[BigDecimal](allValues.size)
-    calc(allValues.tail, allValues.head, rsis, 1, zero, zero).reverse.toList
+    calc(allValues.tail, allValues.head, 1, zero, zero).toList
   }
 }
--- calculations/src/main/scala/currexx/calculations/MovingAverageCalculator.scala
+++ calculations/src/main/scala/currexx/calculations/MovingAverageCalculator.scala
@@ -9,35 +9,35 @@ object MovingAverageCalculator {
 
   private def emaAsArray(values: List[BigDecimal], n: Int, smoothing: BigDecimal = EmaSmoothing): Array[BigDecimal] = {
     val k = smoothing / (1 + n)
+    val allValues = values.reverse
+    val emas = Array.ofDim[BigDecimal](allValues.size)
+    emas(allValues.size-1) = allValues.head
     @tailrec
-    def calc(remainingValues: List[BigDecimal], emas: Array[BigDecimal], i: Int): Array[BigDecimal] =
+    def calc(remainingValues: List[BigDecimal], i: Int): Array[BigDecimal] =
       if (remainingValues.isEmpty) emas
       else {
         val ema = remainingValues.head * k + emas(i) * (1 - k)
         emas.update(i - 1, ema)
-        calc(remainingValues.tail, emas, i - 1)
+        calc(remainingValues.tail, i - 1)
       }
-    val allValues = values.reverse
-    val emas = Array.ofDim[BigDecimal](allValues.size)
-    emas(allValues.size-1) = allValues.head
-    calc(allValues.tail, emas, allValues.size - 1)
+    calc(allValues.tail, allValues.size - 1)
   }
 
   def ema(values: List[BigDecimal], n: Int, smoothing: BigDecimal = EmaSmoothing): List[BigDecimal] =
     emaAsArray(values, n, smoothing).toList
 
   def sma(values: List[BigDecimal], n: Int): List[BigDecimal] = {
+    val smas = Array.ofDim[BigDecimal](values.size)
     @tailrec
-    def calc(smas: Array[BigDecimal], queue: Queue[BigDecimal], remaining: List[BigDecimal], i: Int): List[BigDecimal] =
+    def calc(queue: Queue[BigDecimal], remaining: List[BigDecimal], i: Int): List[BigDecimal] =
       if (remaining.isEmpty) smas.drop(i).toList
-      else if (queue.size < n) calc(smas, queue.addOne(remaining.head), remaining.tail, i)
+      else if (queue.size < n) calc(queue.addOne(remaining.head), remaining.tail, i)
       else {
         val updatedQueue = queue.drop(1).addOne(remaining.head)
         smas(i-1) = updatedQueue.sum / n
-        calc(smas, updatedQueue, remaining.tail, i - 1)
+        calc(updatedQueue, remaining.tail, i - 1)
       }
-    val smas = Array.ofDim[BigDecimal](values.size)
-    calc(smas, Queue.empty, values.reverse, values.size)
+    calc(Queue.empty, values.reverse, values.size)
   }
 
   def macd(values: List[BigDecimal], fastLength: Int = 12, slowLength: Int = 26): List[BigDecimal] = {
