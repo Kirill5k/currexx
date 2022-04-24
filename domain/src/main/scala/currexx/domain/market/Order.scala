fc1fb1c216ba5bd961396b097f739f58864c7eea
kirill5k <immotional@aol.com>
2022-04-24 13:26:47 +0100

    Rename orders

--- domain/src/main/scala/currexx/domain/market/Order.scala
+++ domain/src/main/scala/currexx/domain/market/Order.scala
@@ -19,20 +19,19 @@ object Order {
         .toRight(s"$p is not valid position kind. Accepted values: ${Position.values.map(_.toString.toLowerCase).mkString(", ")}")
     }
 
-
-  final case class MarketOrder(
+  final case class EnterMarket(
       currencyPair: CurrencyPair,
       position: Position,
       volume: BigDecimal,
       stopLoss: Option[BigDecimal],
       trailingStopLoss: Option[BigDecimal],
       takeProfit: Option[BigDecimal]
-  ) extends Order("market")
+  ) extends Order("enter")
       derives Codec.AsObject
 
-  final case class CloseOrder(
+  final case class ExitMarket(
       currencyPair: CurrencyPair
-  ) extends Order("close")
+  ) extends Order("exit")
       derives Codec.AsObject
 
   private val discriminatorField: String            = "kind"
@@ -40,13 +39,13 @@ object Order {
 
   inline given Decoder[Order] = Decoder.instance { c =>
     c.downField(discriminatorField).as[String].flatMap {
-      case "market" => c.as[MarketOrder]
-      case "close"  => c.as[CloseOrder]
-      case kind     => Left(DecodingFailure(s"Unexpected order kind $kind", List(CursorOp.Field(discriminatorField))))
+      case "enter" => c.as[EnterMarket]
+      case "exit"  => c.as[ExitMarket]
+      case kind    => Left(DecodingFailure(s"Unexpected order kind $kind", List(CursorOp.Field(discriminatorField))))
     }
   }
   inline given Encoder[Order] = Encoder.instance {
-    case market: MarketOrder => market.asJson.deepMerge(discriminatorJson(market))
-    case close: CloseOrder   => close.asJson.deepMerge(discriminatorJson(close))
+    case enter: EnterMarket => enter.asJson.deepMerge(discriminatorJson(enter))
+    case exit: ExitMarket   => exit.asJson.deepMerge(discriminatorJson(exit))
   }
 }
