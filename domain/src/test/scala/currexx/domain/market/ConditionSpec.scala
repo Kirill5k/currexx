6e4a96d0dbfad7c2eb74ce011c1642fa610b81f0
kirill5k <immotional@aol.com>
2022-04-24 15:54:44 +0100

    Condition codecs spec

---
+++ domain/src/test/scala/currexx/domain/market/ConditionSpec.scala
@@ -0,0 +1,24 @@
+package currexx.domain.market
+
+import io.circe.syntax.*
+import io.circe.parser.*
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class ConditionSpec extends AnyWordSpec with Matchers{
+
+  "A Condition codecs" should {
+    "decode condition from json" in {
+      val condition: Condition = Condition.CrossingUp
+
+      condition.asJson.noSpaces mustBe """{"kind":"crossing-up"}"""
+    }
+
+    "encode condition to json" in {
+      val json = """{"kind":"above-threshold","threshold":10,"value":20}"""
+
+      decode[Condition](json) mustBe Right(Condition.AboveThreshold(BigDecimal(10), BigDecimal(20)))
+    }
+  }
+}
+
