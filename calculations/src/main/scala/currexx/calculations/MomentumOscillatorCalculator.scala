8efe6d9bf9963945cdb4d12e7644bae92b5e0199
kirill5k <immotional@aol.com>
2022-04-25 15:29:39 +0100

    Stoch oscillator calc tests

--- calculations/src/main/scala/currexx/calculations/MomentumOscillatorCalculator.scala
+++ calculations/src/main/scala/currexx/calculations/MomentumOscillatorCalculator.scala
@@ -5,7 +5,8 @@ import scala.collection.mutable.Queue
 
 object MomentumOscillatorCalculator {
 
-  private val Zero: BigDecimal = BigDecimal(0)
+  private val Zero: BigDecimal    = BigDecimal(0)
+  private val Hundred: BigDecimal = BigDecimal(100.0)
 
   def rsi(values: List[BigDecimal], length: Int): List[BigDecimal] = {
     val rsis = Array.ofDim[BigDecimal](values.size)
@@ -21,7 +22,7 @@ object MomentumOscillatorCalculator {
           val (avgGain, avgLoss) =
             if (i == length) ((gain + currLoss) / length, (loss + currLoss) / length)
             else ((gain * (length - 1) + currGain) / length, (loss * (length - 1) + currLoss) / length)
-          val rsi = 100 - (100 / (1 + avgGain / avgLoss))
+          val rsi = Hundred - (Hundred / (1 + avgGain / avgLoss))
           rsis(values.length - i - 1) = rsi
           calc(remainingValues.tail, remainingValues.head, i + 1, avgGain, avgLoss)
         }
@@ -38,18 +39,18 @@ object MomentumOscillatorCalculator {
       slowKLength: Int,
       slowDLength: Int
   ): (List[BigDecimal], List[BigDecimal]) = {
-    val highsArr    = highs.toArray.reverse
-    val lowsArr     = lows.toArray.reverse
-    val closingsArr = closings.toArray.reverse
-    val stochs      = Array.ofDim[BigDecimal](closings.size-length)
+    val highsArr              = highs.toArray.reverse
+    val lowsArr               = lows.toArray.reverse
+    val closingsArr           = closings.toArray.reverse
+    val stochs                = Array.ofDim[BigDecimal](closings.size - length)
     val hh: Queue[BigDecimal] = Queue.empty
     val ll: Queue[BigDecimal] = Queue.empty
-    var i = 0
+    var i                     = 0
     while (i < closings.length) {
       hh.enqueue(highsArr(i))
       ll.enqueue(lowsArr(i))
       if (i > length) {
-        stochs(i-length) = BigDecimal(100) * ((closingsArr(i) - ll.min) / (hh.max - ll.min))
+        stochs(i - length) = (closingsArr(i) - ll.min) / (hh.max - ll.min) * Hundred
         hh.dequeue()
         ll.dequeue()
       }
