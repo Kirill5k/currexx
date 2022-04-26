c48deb01d32d5aabdd03cf20c3832081cdd9ab37
kirill5k <immotional@aol.com>
2022-04-26 19:52:59 +0100

    Vindaloo settings:

--- core/src/main/scala/currexx/core/market/MarketSettings.scala
+++ core/src/main/scala/currexx/core/market/MarketSettings.scala
@@ -3,7 +3,15 @@ package currexx.core.market
 import currexx.clients.broker.BrokerParameters
 import currexx.domain.user.UserId
 
+final case class TradingParameters(
+    volume: BigDecimal,
+    stopLoss: Option[BigDecimal],
+    trailingStopLoss: Option[BigDecimal],
+    takeProfit: Option[BigDecimal]
+)
+
 final case class MarketSettings(
     userId: UserId,
-    broker: BrokerParameters
+    broker: BrokerParameters,
+    trading: TradingParameters
 )
