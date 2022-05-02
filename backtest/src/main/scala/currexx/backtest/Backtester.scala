c19fecb8c6935f673923aa069d12590a65099e62
kirill5k <immotional@aol.com>
2022-05-02 18:49:08 +0100

    Implement core backtester logic

--- backtest/src/main/scala/currexx/backtest/Backtester.scala
+++ backtest/src/main/scala/currexx/backtest/Backtester.scala
@@ -2,6 +2,7 @@ package currexx.backtest
 
 import cats.effect.{IO, IOApp}
 import currexx.clients.broker.BrokerParameters
+import currexx.core.common.action.{Action, ActionDispatcher, ActionProcessor}
 import currexx.core.market.MarketState
 import currexx.core.signal.SignalSettings
 import currexx.core.trade.{TradeSettings, TradeStrategy, TradingParameters}
@@ -11,6 +12,8 @@ import mongo4cats.bson.ObjectId
 import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
 import squants.market.{EUR, GBP}
+
+import scala.concurrent.duration.*
 
 object Backtester extends IOApp.Simple {
   inline given logger: Logger[IO] = Slf4jLogger.getLogger[IO]
@@ -32,5 +35,22 @@ object Backtester extends IOApp.Simple {
   val tradingParameters = TradingParameters(BigDecimal(0.1), None, None, None)
   val tradeSettings     = TradeSettings(userId, TradeStrategy.HMABasic, BrokerParameters.Vindaloo("1"), tradingParameters)
 
-  override val run: IO[Unit] = ???
+  override val run: IO[Unit] =
+    for
+      dispatcher <- ActionDispatcher.make[IO]
+      market     <- TestMarketService.make[IO](initialMarketState, dispatcher)
+      monitor    <- TestMonitorService.make[IO]
+      trade      <- TestTradeService.make[IO](tradeSettings, dispatcher)
+      signal     <- TestSignalService.make[IO](signalSettings, dispatcher)
+      processor  <- ActionProcessor.make[IO](dispatcher, monitor, signal, market, trade)
+      _ <- TestDataProvider
+        .read[IO](currencyPair, Interval.D1, "eur-gbp-1d.csv")
+        .metered(3.millis)
+        .evalMap(data => dispatcher.dispatch(Action.ProcessMarketData(userId, data)))
+        .concurrently(processor.run)
+        .compile
+        .drain
+      placedOrders <- trade.getAllOrders(userId)
+      _            <- IO.println(s"placed ${placedOrders.size} orders")
+    yield ()
 }
