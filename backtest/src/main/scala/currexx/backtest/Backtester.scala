503e6d29c5643df2faa24204d76bc595d8fc2b29
kirill5k <immotional@aol.com>
2022-05-02 11:17:05 +0100

    Define initial test setup

---
+++ backtest/src/main/scala/currexx/backtest/Backtester.scala
@@ -0,0 +1,36 @@
+package currexx.backtest
+
+import cats.effect.{IO, IOApp}
+import currexx.clients.broker.BrokerParameters
+import currexx.core.market.MarketState
+import currexx.core.signal.SignalSettings
+import currexx.core.trade.{TradeSettings, TradeStrategy, TradingParameters}
+import currexx.domain.market.{CurrencyPair, IndicatorParameters}
+import currexx.domain.user.UserId
+import mongo4cats.bson.ObjectId
+import org.typelevel.log4cats.Logger
+import org.typelevel.log4cats.slf4j.Slf4jLogger
+import squants.market.{GBP, USD}
+
+object Backtester extends IOApp.Simple {
+  inline given logger: Logger[IO] = Slf4jLogger.getLogger[IO]
+
+  val userId       = UserId(ObjectId.get)
+  val currencyPair = CurrencyPair(GBP, USD)
+
+  val initialMarketState = MarketState(userId, currencyPair, None, None, Map.empty, None)
+
+  val signalSettings = SignalSettings(
+    userId,
+    List(
+      IndicatorParameters.HMA(16),
+      IndicatorParameters.MACD(fastLength = 12, slowLength = 26, signalSmoothing = 9),
+      IndicatorParameters.RSI(length = 14, upperLine = 70, lowerLine = 30)
+    )
+  )
+
+  val tradingParameters = TradingParameters(BigDecimal(0.1), None, None, None)
+  val tradeSettings     = TradeSettings(userId, TradeStrategy.HMABasic, BrokerParameters.Vindaloo("1"), tradingParameters)
+
+  override val run: IO[Unit] = ???
+}
