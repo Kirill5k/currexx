af81d2d3be05e38bb317511029ac9695761d6d27
kirill5k <immotional@aol.com>
2022-05-19 20:46:37 +0100

    Kalman g-h-k filter impl

--- calculations/src/main/scala/currexx/calculations/Filters.scala
+++ calculations/src/main/scala/currexx/calculations/Filters.scala
@@ -30,13 +30,13 @@ object Filters {
     calc(values.reverse, initialGuess, math.pow(initializationEstimateError, 2) + noiseVariance, Nil)
   }
 
-  def kalmanWithVelocity(
+  def ghKalman(
       values: List[Double],
       initialGuess: Double,
       initialVelocity: Double,
+      time: Int,
       alpha: Double,
-      beta: Double,
-      time: Int
+      beta: Double
   ): List[Double] = {
     @tailrec
     def calc(
@@ -53,5 +53,36 @@ object Filters {
         calc(measurements.tail, nextEstimate, currentVelocity, currentEstimate :: predictions)
       }
     calc(values.reverse, initialGuess + time * initialVelocity, initialVelocity, Nil)
+  }
+
+  def ghkKalman(
+      values: List[Double],
+      initialGuess: Double,
+      initialVelocity: Double,
+      initialAcceleration: Double,
+      time: Int,
+      alpha: Double,
+      beta: Double,
+      gamma: Double
+  ): List[Double] = {
+    @tailrec
+    def calc(
+        measurements: List[Double],
+        prevEstimate: Double,
+        prevVelocity: Double,
+        prevAcceleration: Double,
+        predictions: List[Double]
+    ): List[Double] =
+      if (measurements.isEmpty) predictions
+      else {
+        val currentEstimate     = prevEstimate + alpha * (measurements.head - prevEstimate)
+        val currentVelocity     = prevVelocity + beta * ((measurements.head - prevEstimate) / time)
+        val currentAcceleration = prevAcceleration + gamma * ((measurements.head - prevEstimate) / (math.pow(time, 2) / 2))
+        val nextEstimate        = currentEstimate + time * currentVelocity + currentAcceleration * (math.pow(time, 2) / 2)
+        val nextVelocity        = currentVelocity + currentAcceleration * time
+        calc(measurements.tail, nextEstimate, nextVelocity, currentAcceleration, currentEstimate :: predictions)
+      }
+    val initialEstimate = initialGuess + time * initialVelocity + initialAcceleration * math.pow(time, 2) / 2
+    calc(values.reverse, initialEstimate, initialVelocity + initialAcceleration * time, initialAcceleration, Nil)
   }
 }
--- calculations/src/test/scala/currexx/calculations/FiltersSpec.scala
+++ calculations/src/test/scala/currexx/calculations/FiltersSpec.scala
@@ -31,7 +31,7 @@ class FiltersSpec extends AnyWordSpec with Matchers {
     }
 
     "use kalman filter for removing noise from measurements of moving objects" in {
-      val result = Filters.kalmanWithVelocity(
+      val result = Filters.ghKalman(
         values = List(30110D, 30265D, 30740D, 30750D, 31135D, 31015D, 31180D, 31610D, 31960D, 31865D).reverse,
         alpha = 0.2,
         beta = 0.1,
@@ -40,7 +40,44 @@ class FiltersSpec extends AnyWordSpec with Matchers {
         time = 5
       )
 
-      result mustBe List(31952.873160384, 31764.32870592, 31529.3570496, 31333.222848, 31176.402240000003, 31001.451200000003, 30769.456000000002, 30573.280000000002, 30351.4, 30182.0)
+      result mustBe List(
+        31952.873160384,
+        31764.32870592,
+        31529.3570496,
+        31333.222848,
+        31176.402240000003,
+        31001.451200000003,
+        30769.456000000002,
+        30573.280000000002,
+        30351.4,
+        30182.0
+      )
+    }
+
+    "use kalman filter for removing noise from measurements of accelerating objects" in {
+      val result = Filters.ghkKalman(
+        values = List(30160D, 30365D, 30890D, 31050D, 31785D, 32215D, 33130D, 34510D, 36010D, 37265D).reverse,
+        alpha = 0.5,
+        beta = 0.4,
+        gamma = 0.1,
+        initialGuess = 30000,
+        initialVelocity = 50,
+        initialAcceleration = 0,
+        time = 5
+      )
+
+      result mustBe List(
+        37465.405750000005,
+        35822.889500000005,
+        34250.6725,
+        33032.54,
+        32201.725,
+        31591.05,
+        31038.75,
+        30721.0,
+        30387.5,
+        30205.0
+      )
     }
   }
 }
