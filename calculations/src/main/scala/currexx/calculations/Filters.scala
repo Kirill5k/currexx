d9bb26f293dd8ddb584b65cac3a6245ddd15d9bd
kirill5k <immotional@aol.com>
2022-05-23 21:06:12 +0100

    Test kalman smoothing

--- calculations/src/main/scala/currexx/calculations/Filters.scala
+++ calculations/src/main/scala/currexx/calculations/Filters.scala
@@ -4,6 +4,32 @@ import scala.annotation.tailrec
 
 object Filters {
 
+  def kalman(
+      values: List[Double],
+      measurementError: Double,           // (0.1)
+      noiseVariance: Double,              // q (0.00011)
+      initialGuess: Double,               // ^x0,0
+      initializationEstimateError: Double // (100)
+  ): List[Double] = {
+    val measurementUncertainty = math.pow(measurementError, 2)
+    @tailrec
+    def calc(
+        measurements: List[Double],
+        currentEstimate: Double,                 // Xn,n
+        extrapolatedEstimateUncertainty: Double, // Pn,n
+        predictions: List[Double]
+    ): List[Double] =
+      if (measurements.isEmpty) predictions
+      else {
+        val measurement             = measurements.head
+        val kalmanGain              = extrapolatedEstimateUncertainty / (extrapolatedEstimateUncertainty + measurementUncertainty)
+        val currentState            = currentEstimate + kalmanGain * (measurement - currentEstimate)
+        val currentStateUncertainty = (1 - kalmanGain) * extrapolatedEstimateUncertainty
+        calc(measurements.tail, currentState, currentStateUncertainty + noiseVariance, currentState :: predictions)
+      }
+    calc(values.reverse, initialGuess, math.pow(initializationEstimateError, 2) + noiseVariance, Nil)
+  }
+
   def ghKalman(
       values: List[Double],
       alpha: Double,
--- calculations/src/test/scala/currexx/calculations/FiltersSpec.scala
+++ calculations/src/test/scala/currexx/calculations/FiltersSpec.scala
@@ -7,6 +7,29 @@ class FiltersSpec extends AnyWordSpec with Matchers {
 
   "A Filters" should {
 
+    "use kalman filter for removing noise from measurements" in {
+      val result = Filters.kalman(
+        values = List(50.45, 50.967, 51.6, 52.106, 52.492, 52.819, 53.433, 54.007, 54.523, 54.99).reverse,
+        measurementError = 0.1,
+        noiseVariance = 0.0001,
+        initialGuess = 10,
+        initializationEstimateError = 100
+      )
+
+      result mustBe List(
+        52.92531824447727,
+        52.62631779251975,
+        52.33076829099416,
+        52.04460275027512,
+        51.77872905021758,
+        51.54805687662509,
+        51.29449410930005,
+        51.01141047992115,
+        50.70976581732428,
+        50.44995955004085
+      )
+    }
+
     "use kalman filter for removing noise from measurements of moving objects" in {
       val result = Filters.ghKalman(
         values = List(30110D, 30265D, 30740D, 30750D, 31135D, 31015D, 31180D, 31610D, 31960D, 31865D).reverse,
