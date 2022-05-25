3de149433fd848d590bc182f803d1bf5bf280d2e
kirill5k <immotional@aol.com>
2022-05-25 20:04:17 +0100

    Parse json from resources

--- core/src/test/scala/currexx/core/FileReader.scala
+++ core/src/test/scala/currexx/core/FileReader.scala
@@ -1,9 +1,16 @@
 package currexx.core
 
+import io.circe.Decoder
+import io.circe.parser.decode
 import scala.io.Source
 
-object FileReader:
+object FileReader {
   def fromResources(path: String): String =
     val source = Source.fromResource(path)
     try source.getLines().toList.mkString
     finally source.close()
+
+  def parseFromResources[A: Decoder](path: String): A =
+    decode[A](fromResources(path)).fold(e => throw new RuntimeException(e.getMessage), identity)
+
+}
