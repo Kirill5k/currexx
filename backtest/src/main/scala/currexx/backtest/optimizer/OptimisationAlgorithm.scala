d7a59d6bfcbb7d99c9f531b73b93986a128c9aa6
kirill5k <immotional@aol.com>
2022-06-16 19:29:10 +0300

    Refactor OptimisationAlgorithm

--- backtest/src/main/scala/currexx/backtest/optimizer/OptimisationAlgorithm.scala
+++ backtest/src/main/scala/currexx/backtest/optimizer/OptimisationAlgorithm.scala
@@ -1,25 +1,27 @@
 package currexx.backtest.optimizer
 
 import cats.effect.Async
+import cats.syntax.functor.*
 import currexx.algorithms.operators.*
 import currexx.algorithms.{Alg, Algorithm, Fitness, Op, Parameters}
 
 import scala.reflect.ClassTag
 import scala.util.Random
 
-trait OptimisationAlgorithm[F[_], A <: Alg, P <: Parameters[A], T]:
-  def optimise(target: T, params: P)(using rand: Random): F[(T, Fitness)]
+trait OptimisationAlgorithm[F[_], A <: Alg, P <: Parameters[A]]:
+  def optimise[T](target: T, params: P)(using rand: Random, opt: Optimisable[T]): F[(T, Fitness)]
 
 object OptimisationAlgorithm:
-  def ga[F[_]: Async, T](
-      initialiser: Initialiser[F, T],
-      crossover: Crossover[F, T],
-      mutator: Mutator[F, T],
-      evaluator: Evaluator[F, T],
-      selector: Selector[F, T],
-      elitism: Elitism[F, T]
-  ): OptimisationAlgorithm[F, Alg.GA, Parameters.GA, T] = new OptimisationAlgorithm[F, Alg.GA, Parameters.GA, T]:
-    override def optimise(target: T, params: Parameters.GA)(using rand: Random): F[(T, Fitness)] =
+  def ga[F[_]: Async](
+      initialiser: Initialiser[F, Array[Array[Int]]],
+      crossover: Crossover[F, Array[Array[Int]]],
+      mutator: Mutator[F, Array[Array[Int]]],
+      evaluator: Evaluator[F, Array[Array[Int]]],
+      selector: Selector[F, Array[Array[Int]]],
+      elitism: Elitism[F, Array[Array[Int]]]
+  ): OptimisationAlgorithm[F, Alg.GA, Parameters.GA] = new OptimisationAlgorithm[F, Alg.GA, Parameters.GA]:
+    override def optimise[T](target: T, params: Parameters.GA)(using rand: Random, opt: Optimisable[T]): F[(T, Fitness)] =
       Algorithm.GA
-        .optimise[T](target, params)
-        .foldMap(Op.ioInterpreter[F, T](initialiser, crossover, mutator, evaluator, selector, elitism, None))
+        .optimise[Array[Array[Int]]](opt.toGenome(target), params)
+        .foldMap(Op.ioInterpreter[F, Array[Array[Int]]](initialiser, crossover, mutator, evaluator, selector, elitism, None))
+        .map((res, fit) => (opt.fromGenome(res), fit))
--- backtest/src/main/scala/currexx/backtest/services/TestMonitorService.scala
+++ backtest/src/main/scala/currexx/backtest/services/TestMonitorService.scala
@@ -7,15 +7,15 @@ import currexx.domain.user.UserId
 final private class TestMonitorService[F[_]](using
     F: Async[F]
 ) extends MonitorService[F]:
-  override def rescheduleAll: F[Unit]                      = F.unit
-  override def create(cm: CreateMonitor): F[MonitorId]     = F.raiseError(new RuntimeException("unimplemented"))
-  override def update(mon: Monitor): F[Unit]               = F.unit
-  override def getAll(uid: UserId): F[List[Monitor]]       = F.pure(Nil)
-  override def get(uid: UserId, id: MonitorId): F[Monitor] = F.raiseError(new RuntimeException("unimplemented"))
-  override def delete(uid: UserId, id: MonitorId): F[Unit] = F.unit
-  override def pause(uid: UserId, id: MonitorId): F[Unit]  = F.unit
-  override def resume(uid: UserId, id: MonitorId): F[Unit] = F.unit
-  override def query(uid: UserId, id: MonitorId): F[Unit]  = F.unit
+  override def rescheduleAll: F[Unit]                                      = F.unit
+  override def create(cm: CreateMonitor): F[MonitorId]                     = F.raiseError(new RuntimeException("unimplemented"))
+  override def update(mon: Monitor): F[Unit]                               = F.unit
+  override def getAll(uid: UserId): F[List[Monitor]]                       = F.pure(Nil)
+  override def get(uid: UserId, id: MonitorId): F[Monitor]                 = F.raiseError(new RuntimeException("unimplemented"))
+  override def delete(uid: UserId, id: MonitorId): F[Unit]                 = F.unit
+  override def pause(uid: UserId, id: MonitorId): F[Unit]                  = F.unit
+  override def resume(uid: UserId, id: MonitorId): F[Unit]                 = F.unit
+  override def query(uid: UserId, id: MonitorId, manual: Boolean): F[Unit] = F.unit
 
 object TestMonitorService:
   def make[F[_]: Async]: F[MonitorService[F]] =
