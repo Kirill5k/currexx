80f6f5db61299b6e51d30586d53d43834eafdcfd
kirill5k <immotional@aol.com>
2022-06-17 10:41:21 +0300

    Mutator.bitFlip impl and tests

--- algorithms/src/main/scala/currexx/algorithms/operators/Mutator.scala
+++ algorithms/src/main/scala/currexx/algorithms/operators/Mutator.scala
@@ -8,8 +8,27 @@ trait Mutator[F[_], I]:
   def mutate(ind: I, mutationProbability: Double)(using r: Random): F[I]
 
 object Mutator:
+  def bitFlip[F[_]: Sync]: F[Mutator[F, Array[Int]]] =
+    Sync[F].pure {
+      new Mutator[F, Array[Int]] {
+        override def mutate(ind: Array[Int], mutationProbability: Double)(using r: Random): F[Array[Int]] =
+          Sync[F].delay {
+            val result = ind.clone()
+            var i      = 0
+            while (i < result.length - 1) {
+              if (r.nextDouble() < mutationProbability) {
+                val curr = result(i)
+                result(i) = if (curr == 0) 1 else 0
+              }
+              i += 1
+            }
+            result
+          }
+      }
+    }
+
   def neighbourSwap[F[_]: Sync, G]: F[Mutator[F, Array[G]]] =
-    Sync[F].delay {
+    Sync[F].pure {
       new Mutator[F, Array[G]] {
         override def mutate(ind: Array[G], mutationFactor: Double)(using r: Random): F[Array[G]] =
           Sync[F].delay {
--- algorithms/src/test/scala/currexx/algorithms/operators/MutatorSpec.scala
+++ algorithms/src/test/scala/currexx/algorithms/operators/MutatorSpec.scala
@@ -11,13 +11,26 @@ class MutatorSpec extends CatsSpec {
     "swap 2 neighbour elements in seq" in {
       val individual = Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
-      given r: Random   = Random(40)
-      val result = Mutator.neighbourSwap[IO, Int].flatMap(_.mutate(individual, 0.25))
+      given Random = Random(40)
+      val result   = Mutator.neighbourSwap[IO, Int].flatMap(_.mutate(individual, 0.25))
 
       result.asserting { mutated =>
         mutated must not contain theSameElementsInOrderAs(individual)
         mutated must contain theSameElementsAs individual
-        mutated must contain theSameElementsInOrderAs Array(1, 3, 2, 4, 5, 7, 6, 9, 8, 10)
+        (mutated must contain).theSameElementsInOrderAs(Array(1, 3, 2, 4, 5, 7, 6, 9, 8, 10))
+      }
+    }
+  }
+
+  "Mutator.bitFlip" should {
+    "flip arbitrary bit in a genetic sequence from its original state" in {
+      val individual = Array(0, 1, 0, 1, 0, 1, 0, 1)
+
+      given r: Random = Random(40)
+      val result      = Mutator.bitFlip[IO].flatMap(_.mutate(individual, 0.25))
+
+      result.asserting { mutated =>
+        mutated mustBe Array(0, 0, 0, 1, 0, 0, 0, 1)
       }
     }
   }
