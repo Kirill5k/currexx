d3570f95ade025fc17a9f76752055bfab5f651e3
kirill5k <immotional@aol.com>
2022-07-21 20:10:04 +0300

    Instant codec

--- core/src/main/scala/currexx/core/common/http/TapirCodecs.scala
+++ core/src/main/scala/currexx/core/common/http/TapirCodecs.scala
@@ -3,12 +3,17 @@ package currexx.core.common.http
 import squants.market.{Currency, defaultMoneyContext}
 import sttp.tapir.Codec.PlainCodec
 import sttp.tapir.{Codec, DecodeResult, ValidationError, Validator}
+import currexx.core.common.time.*
+
+import java.time.Instant
 
 transparent trait TapirCodecs {
 
   private def decodeCurrency(code: String): DecodeResult[Currency] =
     Currency(code)(defaultMoneyContext).fold(DecodeResult.Error(code, _), DecodeResult.Value.apply)
 
-  implicit val currencyCodec: PlainCodec[Currency] =
-    Codec.string.mapDecode[Currency](decodeCurrency)(_.code)
+  inline given PlainCodec[Currency] = Codec.string.mapDecode[Currency](decodeCurrency)(_.code)
+
+  inline given instantCodec: PlainCodec[Instant] =
+    Codec.string.mapDecode(d => d.toInstant.fold(DecodeResult.Error(d, _), DecodeResult.Value(_)))(_.toString)
 }
--- core/src/main/scala/currexx/core/common/time.scala
+++ core/src/main/scala/currexx/core/common/time.scala
@@ -5,6 +5,14 @@ import scala.concurrent.duration.*
 import scala.util.Try
 
 object time:
+  extension (dateString: String)
+    def toInstant: Either[Throwable, Instant] =
+      val localDate = dateString.length match
+        case 10 => s"${dateString}T00:00:00Z"
+        case 19 => s"${dateString}Z"
+        case _  => dateString
+      Try(Instant.parse(localDate)).toEither
+
   extension (ld: LocalDate) def toInstantAtStartOfDay: Instant = ld.atStartOfDay().toInstant(ZoneOffset.UTC)
   extension (ts: Instant)
     def toLocalDate: LocalDate = LocalDate.parse(ts.toString.slice(0, 10))
