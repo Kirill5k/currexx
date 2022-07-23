2a7ccc3b2359140367124719117e8b3c7884dda4
kirill5k <immotional@aol.com>
2022-07-23 16:26:40 +0300

    Currency pair codec

--- core/src/main/scala/currexx/core/common/http/TapirCodecs.scala
+++ core/src/main/scala/currexx/core/common/http/TapirCodecs.scala
@@ -4,6 +4,8 @@ import squants.market.{Currency, defaultMoneyContext}
 import sttp.tapir.Codec.PlainCodec
 import sttp.tapir.{Codec, DecodeResult, ValidationError, Validator}
 import currexx.core.common.time.*
+import currexx.domain.errors.AppError
+import currexx.domain.market.CurrencyPair
 
 import java.time.Instant
 
@@ -12,7 +14,12 @@ transparent trait TapirCodecs {
   private def decodeCurrency(code: String): DecodeResult[Currency] =
     Currency(code)(defaultMoneyContext).fold(DecodeResult.Error(code, _), DecodeResult.Value.apply)
 
+  private def decodeCurrencyPair(cp: String): DecodeResult[CurrencyPair] =
+    CurrencyPair.from(cp).left.map(AppError.FailedValidation.apply).fold(DecodeResult.Error(cp, _), DecodeResult.Value.apply)
+
   inline given PlainCodec[Currency] = Codec.string.mapDecode[Currency](decodeCurrency)(_.code)
+
+  inline given PlainCodec[CurrencyPair] = Codec.string.mapDecode[CurrencyPair](decodeCurrencyPair)(_.toString)
 
   inline given instantCodec: PlainCodec[Instant] =
     Codec.string.mapDecode(d => d.toInstant.fold(DecodeResult.Error(d, _), DecodeResult.Value(_)))(_.toString)
--- domain/src/main/scala/currexx/domain/market/CurrencyPair.scala
+++ domain/src/main/scala/currexx/domain/market/CurrencyPair.scala
@@ -5,15 +5,20 @@ import io.circe.{Decoder, Encoder}
 import squants.market.{Currency, defaultMoneyContext}
 
 final case class CurrencyPair(base: Currency, quote: Currency):
-  def toSymbol: String = s"${base.code}${quote.code}"
+  def toSymbol: String          = s"${base.code}${quote.code}"
   override def toString: String = s"${base.code}/${quote.code}"
 
 object CurrencyPair:
-  inline given Encoder[CurrencyPair] = Encoder.encodeString.contramap(_.toString)
-  inline given Decoder[CurrencyPair] = Decoder.decodeString.emap { cp =>
+  inline def from(strRepr: String): Either[String, CurrencyPair] =
     for
-      pair  <- Either.cond(cp.matches("^[A-Z]{3}\\/[A-Z]{3}$"), cp.split("/"), s"$cp is not valid currency pair representation")
+      pair <- Either.cond(
+        strRepr.matches("^[A-Z]{3}\\/[A-Z]{3}$"),
+        strRepr.split("/"),
+        s"$strRepr is not valid currency pair representation"
+      )
       base  <- Currency(pair.head)(defaultMoneyContext).toEither.leftMap(_.getMessage)
       quote <- Currency(pair.last)(defaultMoneyContext).toEither.leftMap(_.getMessage)
     yield CurrencyPair(base, quote)
-  }
+
+  inline given Encoder[CurrencyPair] = Encoder.encodeString.contramap(_.toString)
+  inline given Decoder[CurrencyPair] = Decoder.decodeString.emap(CurrencyPair.from(_))
