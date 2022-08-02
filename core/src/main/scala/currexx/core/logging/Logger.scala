1d3b1a36ade9f52d63d433822278cf8af2159453
kirill5k <immotional@aol.com>
2022-08-02 21:12:19 +0300

    Store logged events in queue

---
+++ core/src/main/scala/currexx/core/logging/Logger.scala
@@ -0,0 +1,75 @@
+package currexx.core.logging
+
+import cats.effect.{Async, Temporal}
+import cats.effect.std.Queue
+import cats.syntax.apply.*
+import cats.syntax.flatMap.*
+import cats.syntax.functor.*
+import fs2.Stream
+import org.typelevel.log4cats.slf4j.Slf4jLogger
+import org.typelevel.log4cats.Logger as Logger4Cats
+
+import java.time.Instant
+
+private[logging] enum LogLevel:
+  case Trace, Debug, Info, Warn, Error
+
+final private[logging] case class LoggedEvent(level: LogLevel, time: Instant, message: String)
+
+trait Logger[F[_]] extends Logger4Cats[F] {
+  def events: Stream[F, LoggedEvent]
+}
+
+final private class LiveLogger[F[_]](
+    private val logger: Logger4Cats[F],
+    private val loggedEvents: Queue[F, LoggedEvent]
+)(using
+    F: Temporal[F]
+) extends Logger[F] {
+
+  private def enqueue(level: LogLevel, t: Throwable, message: => String): F[Unit] =
+    enqueue(level, s"${message.split("\n").head} - ${t.getMessage}")
+
+  private def enqueue(level: LogLevel, message: => String): F[Unit] =
+    F.realTimeInstant.flatMap(t => loggedEvents.offer(LoggedEvent(level, t, message.split("\n").head)))
+
+  override def events: Stream[F, LoggedEvent] =
+    Stream.fromQueueUnterminated(loggedEvents)
+
+  override def error(t: Throwable)(message: => String): F[Unit] =
+    enqueue(LogLevel.Error, t, message) *> logger.error(t)(message)
+
+  override def error(message: => String): F[Unit] =
+    enqueue(LogLevel.Error, message) *> logger.error(message)
+
+  override def warn(t: Throwable)(message: => String): F[Unit] =
+    enqueue(LogLevel.Warn, t, message) *> logger.warn(t)(message)
+
+  override def info(t: Throwable)(message: => String): F[Unit] =
+    enqueue(LogLevel.Info, t, message) *> logger.info(t)(message)
+
+  override def debug(t: Throwable)(message: => String): F[Unit] =
+    enqueue(LogLevel.Debug, t, message) *> logger.debug(t)(message)
+
+  override def trace(t: Throwable)(message: => String): F[Unit] =
+    enqueue(LogLevel.Trace, t, message) *> logger.trace(t)(message)
+
+  override def warn(message: => String): F[Unit] =
+    enqueue(LogLevel.Warn, message) *> logger.warn(message)
+
+  override def info(message: => String): F[Unit] =
+    enqueue(LogLevel.Info, message) *> logger.info(message)
+
+  override def debug(message: => String): F[Unit] =
+    enqueue(LogLevel.Debug, message) *> logger.debug(message)
+
+  override def trace(message: => String): F[Unit] =
+    enqueue(LogLevel.Trace, message) *> logger.trace(message)
+}
+
+object Logger {
+  def apply[F[_]](using ev: Logger[F]): Logger[F] = ev
+
+  def make[F[_]: Async]: F[Logger[F]] =
+    Queue.unbounded[F, LoggedEvent].map(q => LiveLogger[F](Slf4jLogger.getLogger[F], q))
+}
