4c312b7ab3a688d5f13f1cbcc6af1a23b318d8cb
kirill5k <immotional@aol.com>
2022-09-07 17:49:03 +0100

    Update mongodb init

--- core/src/main/scala/currexx/core/Resources.scala
+++ core/src/main/scala/currexx/core/Resources.scala
@@ -5,11 +5,13 @@ import cats.syntax.apply.*
 import currexx.core.common.config.{AppConfig, MongoConfig}
 import mongo4cats.client.MongoClient
 import mongo4cats.database.MongoDatabase
+import mongo4cats.models.client.{ConnectionString, MongoClientSettings}
 import sttp.capabilities.WebSockets
 import sttp.capabilities.fs2.Fs2Streams
 import sttp.client3.asynchttpclient.fs2.AsyncHttpClientFs2Backend
 import sttp.client3.{SttpBackend, SttpBackendOptions}
 
+import java.util.concurrent.TimeUnit
 import scala.concurrent.duration.*
 
 final class Resources[F[_]] private (
@@ -18,10 +20,19 @@ final class Resources[F[_]] private (
 )
 
 object Resources:
-  private def mongoDb[F[_]: Async](config: MongoConfig): Resource[F, MongoDatabase[F]] =
-    MongoClient
-      .fromConnectionString[F](config.connectionUri)
-      .evalMap(_.getDatabase("currexx"))
+
+  private def mongoDb[F[_] : Async](config: MongoConfig): Resource[F, MongoDatabase[F]] =
+    val settings = MongoClientSettings
+      .builder()
+      .applyConnectionString(ConnectionString(config.connectionUri))
+      .applyToSocketSettings { builder =>
+        builder.connectTimeout(3, TimeUnit.MINUTES).readTimeout(3, TimeUnit.MINUTES)
+      }
+      .applyToClusterSettings { builder =>
+        builder.serverSelectionTimeout(3, TimeUnit.MINUTES)
+      }
+      .build()
+    MongoClient.create[F](settings).evalMap(_.getDatabase("currexx"))
 
   private def sttpBackend[F[_]: Async]: Resource[F, SttpBackend[F, Fs2Streams[F] with WebSockets]] =
     AsyncHttpClientFs2Backend.resource[F](SttpBackendOptions(connectionTimeout = 3.minutes, proxy = None))
