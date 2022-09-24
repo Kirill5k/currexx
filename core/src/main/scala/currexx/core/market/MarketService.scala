060aa1d06a3fb472670eb99148f56f4b3dfafc23
kirill5k <immotional@aol.com>
2022-09-24 08:50:19 +0100

    Cleanup services

--- core/src/main/scala/currexx/core/market/MarketService.scala
+++ core/src/main/scala/currexx/core/market/MarketService.scala
@@ -31,34 +31,37 @@ final private class LiveMarketService[F[_]](
 
   override def clearState(uid: UserId, cp: CurrencyPair, closePendingOrders: Boolean): F[Unit] =
     stateRepo.delete(uid, cp) >> F.whenA(closePendingOrders)(dispatcher.dispatch(Action.CloseOpenOrders(uid, cp)))
-  
+
   override def processTradeOrderPlacement(top: TradeOrderPlacement): F[Unit] = {
     val position = top.order match
-      case enter: TradeOrder.Enter => Some(enter.position)
+      case enter: TradeOrder.Enter => Some(PositionState(enter.position, top.time, top.price))
       case TradeOrder.Exit         => None
-    stateRepo.update(top.userId, top.currencyPair, position.map(p => PositionState(p, top.time, top.price))).void
+    stateRepo.update(top.userId, top.currencyPair, position).void
   }
 
   override def processSignals(uid: UserId, cp: CurrencyPair, signals: List[Signal]): F[Unit] =
-    stateRepo.find(uid, cp).flatMap { state =>
-      val existingSignals = state.fold(Map.empty[String, List[IndicatorState]])(_.signals)
+    stateRepo
+      .find(uid, cp)
+      .flatMap { state =>
+        val existingSignals = state.fold(Map.empty[String, List[IndicatorState]])(_.signals)
 
-      val updatedSignals = signals.foldLeft(existingSignals) { (current, signal) =>
-        val indicatorStates   = current.getOrElse(signal.triggeredBy.kind, Nil)
-        val newIndicatorState = IndicatorState(signal.condition, signal.time, signal.triggeredBy)
-        val updatedIndicatorStates =
-          if (indicatorStates.isEmpty) List(newIndicatorState)
-          else if (indicatorStates.head.time.hasSameDateAs(signal.time)) newIndicatorState :: indicatorStates.tail
-          else newIndicatorState :: indicatorStates.take(5)
-        current + (signal.triggeredBy.kind -> updatedIndicatorStates)
+        val updatedSignals = signals.foldLeft(existingSignals) { (current, signal) =>
+          val indicatorStates   = current.getOrElse(signal.triggeredBy.kind, Nil)
+          val newIndicatorState = IndicatorState(signal.condition, signal.time, signal.triggeredBy)
+          val updatedIndicatorStates =
+            if (indicatorStates.isEmpty) List(newIndicatorState)
+            else if (indicatorStates.head.time.hasSameDateAs(signal.time)) newIndicatorState :: indicatorStates.tail
+            else newIndicatorState :: indicatorStates.take(5)
+          current + (signal.triggeredBy.kind -> updatedIndicatorStates)
+        }
+
+        if (existingSignals == updatedSignals) F.pure(None)
+        else stateRepo.update(uid, cp, updatedSignals).map(Some(_))
       }
-
-      if (existingSignals == updatedSignals) F.pure(None)
-      else stateRepo.update(uid, cp, updatedSignals).map(Some(_))
-    }.flatMap {
-      case Some(state) => dispatcher.dispatch(Action.ProcessMarketStateUpdate(state, signals.map(_.triggeredBy)))
-      case None => F.unit
-    }
+      .flatMap {
+        case Some(state) => dispatcher.dispatch(Action.ProcessMarketStateUpdate(state, signals.map(_.triggeredBy)))
+        case None        => F.unit
+      }
 }
 
 object MarketService:
