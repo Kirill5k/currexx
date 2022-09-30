55179fc009a0a153c0775534e087fcab5847e9ef
kirill5k <immotional@aol.com>
2022-09-30 08:45:24 +0100

    PrePadTo implementation

--- backtest/src/main/scala/currexx/backtest/optimizer/syntax.scala
+++ backtest/src/main/scala/currexx/backtest/optimizer/syntax.scala
@@ -1,9 +1,17 @@
 package currexx.backtest.optimizer
 
 object syntax {
+  extension (s: String)
+    def prePadTo(maxLength: Int, char: Char): String =
+      if (s.length >= maxLength) s
+      else {
+        val remainingLength = maxLength - s.length
+        char.toString * remainingLength + s
+      }
+
   extension (array: Array[Int]) def toInt: Int = Integer.parseInt(array.mkString(""), 2)
 
   extension (i: Int)
     def toBinaryArray(maxValue: Int): Array[Int] =
-      i.toBinaryString.reverse.padTo(maxValue.toBinaryString.length, '0').reverse.map(_.toString.toInt).toArray
+      i.toBinaryString.prePadTo(maxValue.toBinaryString.length, '0').map(_.toString.toInt).toArray
 }
