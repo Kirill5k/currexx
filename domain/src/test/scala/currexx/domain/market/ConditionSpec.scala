438701ac541f0fbf3c28a353a75071c1dbc10e35
kirill5k <immotional@aol.com>
2022-10-04 20:05:54 +0100

    Fix test

--- domain/src/test/scala/currexx/domain/market/ConditionSpec.scala
+++ domain/src/test/scala/currexx/domain/market/ConditionSpec.scala
@@ -7,29 +7,54 @@ import org.scalatest.wordspec.AnyWordSpec
 
 class ConditionSpec extends AnyWordSpec with Matchers {
 
-  "Condition codecs" should {
-    "decode crossing-up condition from json" in {
-      val condition: Condition = Condition.CrossingUp
-      val json = """{"kind":"crossing-up"}"""
+  "A Condition" when {
+    "working with codecs" should {
+      "decode crossing-up condition from json" in {
+        val condition: Condition = Condition.CrossingUp
+        val json = """{"kind":"crossing-up"}"""
 
-      condition.asJson.noSpaces mustBe json
-      decode[Condition](json) mustBe Right(condition)
+        condition.asJson.noSpaces mustBe json
+        decode[Condition](json) mustBe Right(condition)
+      }
+
+      "decode above-threshold condition from json" in {
+        val condition: Condition = Condition.AboveThreshold(BigDecimal(10), BigDecimal(20))
+        val json = """{"threshold":10,"value":20,"kind":"above-threshold"}"""
+
+        condition.asJson.noSpaces mustBe json
+        decode[Condition](json) mustBe Right(condition)
+      }
+
+      "decode trend-direction-change condition from json" in {
+        val condition: Condition = Condition.TrendDirectionChange(Trend.Upward, Trend.Downward)
+        val json = """{"from":"upward","to":"downward","previousTrendLength":null,"kind":"trend-direction-change"}"""
+
+        condition.asJson.noSpaces mustBe json
+        decode[Condition](json) mustBe Right(condition)
+      }
     }
 
-    "decode above-threshold condition from json" in {
-      val condition: Condition = Condition.AboveThreshold(BigDecimal(10), BigDecimal(20))
-      val json = """{"threshold":10,"value":20,"kind":"above-threshold"}"""
+    "linesCrossing" should {
+      "return CrossingDown when line 1 crosses line 2 from above" in {
+        val line1 = List(1.0, 3.0, 3.0, 3.0, 3.0)
+        val line2 = List(3.0, 1.0, 1.0, 1.0, 1.0)
 
-      condition.asJson.noSpaces mustBe json
-      decode[Condition](json) mustBe Right(condition)
-    }
+        Condition.linesCrossing(line1, line2) mustBe Some(Condition.CrossingDown)
+      }
 
-    "decode trend-direction-change condition from json" in {
-      val condition: Condition = Condition.TrendDirectionChange(Trend.Upward, Trend.Downward)
-      val json = """{"from":"upward","to":"downward","previousTrendLength":null,"kind":"trend-direction-change"}"""
+      "return CrossingUp when line 1 crosses line 2 from below" in {
+        val line1 = List(3.0, 1.0, 1.0, 1.0, 1.0)
+        val line2 = List(1.0, 3.0, 3.0, 3.0, 3.0)
 
-      condition.asJson.noSpaces mustBe json
-      decode[Condition](json) mustBe Right(condition)
+        Condition.linesCrossing(line1, line2) mustBe Some(Condition.CrossingUp)
+      }
+
+      "return None when lines do not intersect" in {
+        val line1 = List(2.0, 1.0, 1.0, 1.0, 1.0)
+        val line2 = List(3.0, 3.0, 3.0, 3.0, 3.0)
+
+        Condition.linesCrossing(line1, line2) mustBe None
+      }
     }
   }
 }
