3cac5ad8b107bf2f1b929e2dd2661fbbba43da1d
kirill5k <immotional@aol.com>
2022-10-05 09:34:52 +0100

    Test for trendDirectionChange

--- domain/src/test/scala/currexx/domain/market/ConditionSpec.scala
+++ domain/src/test/scala/currexx/domain/market/ConditionSpec.scala
@@ -8,10 +8,10 @@ import org.scalatest.wordspec.AnyWordSpec
 class ConditionSpec extends AnyWordSpec with Matchers {
 
   "A Condition" when {
-    "working with codecs" should {
+    "working with json codecs" should {
       "decode crossing-up condition from json" in {
         val condition: Condition = Condition.CrossingUp
-        val json = """{"kind":"crossing-up"}"""
+        val json                 = """{"kind":"crossing-up"}"""
 
         condition.asJson.noSpaces mustBe json
         decode[Condition](json) mustBe Right(condition)
@@ -19,7 +19,7 @@ class ConditionSpec extends AnyWordSpec with Matchers {
 
       "decode above-threshold condition from json" in {
         val condition: Condition = Condition.AboveThreshold(BigDecimal(10), BigDecimal(20))
-        val json = """{"threshold":10,"value":20,"kind":"above-threshold"}"""
+        val json                 = """{"threshold":10,"value":20,"kind":"above-threshold"}"""
 
         condition.asJson.noSpaces mustBe json
         decode[Condition](json) mustBe Right(condition)
@@ -27,10 +27,30 @@ class ConditionSpec extends AnyWordSpec with Matchers {
 
       "decode trend-direction-change condition from json" in {
         val condition: Condition = Condition.TrendDirectionChange(Trend.Upward, Trend.Downward)
-        val json = """{"from":"upward","to":"downward","previousTrendLength":null,"kind":"trend-direction-change"}"""
+        val json                 = """{"from":"upward","to":"downward","previousTrendLength":null,"kind":"trend-direction-change"}"""
 
         condition.asJson.noSpaces mustBe json
         decode[Condition](json) mustBe Right(condition)
+      }
+    }
+
+    "thresholdCrossing" should {
+      "return AboveThreshold when current value is above max" in {
+        val line = List(5.0, 1.0, 1.0, 1.0, 1.0)
+
+        Condition.thresholdCrossing(line, 1.0, 4.0) mustBe Some(Condition.AboveThreshold(4.0, 5.0))
+      }
+
+      "return BelowThreshold when current value is below min" in {
+        val line = List(0.5, 1.0, 1.0, 1.0, 1.0)
+
+        Condition.thresholdCrossing(line, 1.0, 4.0) mustBe Some(Condition.BelowThreshold(1.0, 0.5))
+      }
+
+      "return None when value is within limits" in {
+        val line = List(3.0, 5.0, 1.0, 1.0, 1.0)
+
+        Condition.thresholdCrossing(line, 1.0, 4.0) mustBe None
       }
     }
 
@@ -54,6 +74,14 @@ class ConditionSpec extends AnyWordSpec with Matchers {
         val line2 = List(3.0, 3.0, 3.0, 3.0, 3.0)
 
         Condition.linesCrossing(line1, line2) mustBe None
+      }
+    }
+
+    "trendDirectionChange" should {
+      "return TrendDirectionChange when trend changes from Upward to Consolidation" in {
+        val line = List(1.1522, 1.1464, 1.1346, 1.1239, 1.1134, 1.1109, 1.1177, 1.1339, 1.1443, 1.1417, 1.1382, 1.1393)
+
+        Condition.trendDirectionChange(line) mustBe Some(Condition.TrendDirectionChange(Trend.Upward, Trend.Consolidation, Some(4)))
       }
     }
   }
