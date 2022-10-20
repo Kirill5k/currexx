e69f4f84f9ed4d2cecd4e06e40d554be8775b54a
kirill5k <immotional@aol.com>
2022-10-20 14:44:19 +0300

    RSX implementation

--- calculations/src/main/scala/currexx/calculations/MomentumOscillators.scala
+++ calculations/src/main/scala/currexx/calculations/MomentumOscillators.scala
@@ -4,23 +4,7 @@ import scala.annotation.tailrec
 import scala.collection.mutable.{Queue => MQueue}
 
 object MomentumOscillators {
-
-  def aroon(values: List[Double], length: Int): List[Double] = {
-    val res = Array.ofDim[Double](values.size)
-    def calc(remainingValues: List[Double], i: Int, window: MQueue[Double]): List[Double] =
-      if (remainingValues.isEmpty) res.take(values.size - length).toList
-      else if (window.size < length) calc(remainingValues.tail, i - 1, window.enqueue(remainingValues.head))
-      else {
-        val max  = window.zipWithIndex.maxBy(_._1)
-        val min  = window.zipWithIndex.minBy(_._1)
-        val up   = 100.0 * (max._2 + 1) / length
-        val down = 100.0 * (min._2 + 1) / length
-        res(i) = up - down
-        calc(remainingValues.tail, i - 1, window.drop(1).enqueue(remainingValues.head))
-      }
-    calc(values.reverse, values.length - 1, MQueue.empty)
-  }
-
+  
   def relativeStrengthIndex(values: List[Double], length: Int): List[Double] = {
     val rsis = Array.ofDim[Double](values.size)
     @tailrec
@@ -72,5 +56,81 @@ object MomentumOscillators {
     val k = MovingAverages.simple(stochs.reverse.toList, slowKLength)
     val d = MovingAverages.simple(k, slowDLength)
     (k, d)
+  }
+
+  def jurikRelativeStrengthIndex(values: List[Double], length: Int): List[Double] = {
+    val f18 = 3.0 / (length + 2)
+    val f20 = 1.0 - f18
+
+    @tailrec
+    def go(
+        remaining: List[Double],
+        result: List[Double],
+        prevF8: Double,
+        prevF28: Double,
+        prevF30: Double,
+        prevF38: Double,
+        prevF40: Double,
+        prevF48: Double,
+        prevF50: Double,
+        prevF58: Double,
+        prevF60: Double,
+        prevF68: Double,
+        prevF70: Double,
+        prevF78: Double,
+        prevF80: Double,
+        prevF88: Double,
+        prevF90: Double
+    ): List[Double] =
+      if (remaining.isEmpty) result
+      else {
+        val f8  = 100 * remaining.head
+        val f10 = prevF8
+        val v8  = f8 - f10
+        val f28 = f20 * prevF28 + f18 * v8
+        val f30 = f18 + f28 + f20 * prevF30
+        val vC  = f28 * 1.5 - f30 * 0.5
+        val f38 = f20 * prevF38 + f18 * vC
+        val f40 = f18 * f38 + f20 * prevF40
+        val v10 = f38 * 1.5 - f40 * 0.5
+        val f48 = f20 * prevF48 + f18 * v10
+        val f50 = f18 * f48 + f20 * prevF50
+        val v14 = f48 * 1.5 - f50 * 0.5
+        val f58 = f20 * prevF58 + f18 * math.abs(v8)
+        val f60 = f18 * f58 + f20 * prevF60
+        val v18 = f58 * 1.5 - f60 * 0.5
+        val f68 = f20 * prevF68 + f18 * v18
+        val f70 = f18 * f68 + f20 * prevF70
+        val v1C = f68 * 1.5 - f70 * 0.5
+        val f78 = f20 * prevF78 + f18 * v1C
+        val f80 = f18 * f78 + f20 * prevF80
+        val v20 = f78 * 1.5 - f80 * 0.5
+        val f90 = if (prevF90 == 0) 1 else if (prevF88 <= prevF90) prevF88 + 1 else prevF90 + 1
+        val f88 = if (prevF90 == 0 && (length - 1) >= 5) length - 1 else 5
+        val f0  = if (f88 >= f90 && f8 != f10) 1 else 0
+        val f91 = if (f88 == f90 && f0 == 0) 0 else f90
+        val v4  = if (f88 < f91 && v20 > 0) (v14 / v20 + 1) * 50 else 50
+        val rsx = if (v4 > 100) 100 else if (v4 < 0) 0 else v4
+        go(
+          remaining.tail,
+          rsx :: result,
+          f8,
+          f28,
+          f30,
+          f38,
+          f40,
+          f48,
+          f50,
+          f58,
+          f60,
+          f68,
+          f70,
+          f78,
+          f80,
+          f88,
+          f90
+        )
+      }
+    go(values.reverse, Nil, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
   }
 }
