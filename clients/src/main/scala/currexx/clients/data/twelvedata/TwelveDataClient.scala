d565b53df05a8efacf9b0d0c0342919f07ae6a2d
kirill5k <immotional@aol.com>
2022-11-08 17:49:43 +0000

    Refactor TwelveDataClient

--- clients/src/main/scala/currexx/clients/data/twelvedata/TwelveDataClient.scala
+++ clients/src/main/scala/currexx/clients/data/twelvedata/TwelveDataClient.scala
@@ -38,31 +38,38 @@ final private class LiveTwelveDataClient[F[_]](
   override protected val delayBetweenConnectionFailures: FiniteDuration = 5.seconds
 
   override def timeSeriesData(pair: CurrencyPair, interval: Interval): F[MarketTimeSeriesData] =
+    fetchTimeSeriesData(pair, interval, 150)
+
+  override def latestPrice(pair: CurrencyPair): F[PriceRange] =
+    fetchTimeSeriesData(pair, Interval.M1, 1).map(_.prices.head)
+
+  private def fetchTimeSeriesData(pair: CurrencyPair, interval: Interval, numOfTicks: Int): F[MarketTimeSeriesData] =
     val sym = s"${pair.base}/${pair.quote}"
     val int = s"${interval.number}/${if (interval.unit == "hour") "h" else interval.unit.slice(0, 3)}"
-    val uri = uri"${config.baseUri}/time_series?symbol=$sym&interval=$int&apikey=${config.apiKey}&outputsize=150"
+    val uri = uri"${config.baseUri}/time_series?symbol=$sym&interval=$int&apikey=${config.apiKey}&outputsize=$numOfTicks"
     dispatch(basicRequest.get(uri).response(asJson[TimeSeriesResponse]))
       .flatMap { r =>
-        r.body match {
+        r.body match
           case Right(res) =>
             val prices = res.values.zipWithIndex.map((v, i) => PriceRange(v.open, v.high, v.low, v.close, 0d, v.datetime.toInstant(i)))
             MarketTimeSeriesData(pair, interval, prices).pure[F]
-          case Left(DeserializationException(_, _)) =>
-            F.sleep(1.minute) >>  timeSeriesData(pair, interval)
+          case Left(DeserializationException(responseBody, error)) =>
+            if (responseBody.contains("\"code\": 429"))
+              F.sleep(1.minute) >> fetchTimeSeriesData(pair, interval, numOfTicks)
+            else
+              logger.error(s"$name-client/json-parsing: ${error.getMessage}\n$responseBody") >>
+                F.raiseError(AppError.JsonParsingFailure(responseBody, s"Failed to parse $name response: ${error.getMessage}"))
           case Left(HttpError(responseBody, StatusCode.Forbidden)) =>
             logger.error(s"$name-client/forbidden\n$responseBody") >>
               F.raiseError(AppError.AccessDenied(s"$name authentication has expired"))
           case Left(HttpError(responseBody, status)) =>
             logger.error(s"$name-client/${status.code}\n$responseBody") >>
-              F.sleep(delayBetweenConnectionFailures) >> timeSeriesData(pair, interval)
-        }
+              F.sleep(delayBetweenConnectionFailures) >> fetchTimeSeriesData(pair, interval, numOfTicks)
       }
 
   extension (ld: LocalDate)
     def toInstant(i: Int): Instant =
-      if (i > 0) ld.atStartOfDay().toInstant(ZoneOffset.UTC) else Instant.now
-
-  override def latestPrice(currencyPair: CurrencyPair): F[PriceRange] = ???
+      if (i == 0) ld.atTime(LocalTime.now()).toInstant(ZoneOffset.UTC) else ld.atStartOfDay().toInstant(ZoneOffset.UTC)
 }
 
 private[clients] object TwelveDataClient {
