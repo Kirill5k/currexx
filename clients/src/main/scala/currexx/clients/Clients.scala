f3d9ac85aaadaa70129d5f78ac563ca541c57cd1
kirill5k <immotional@aol.com>
2022-11-09 14:17:20 +0000

    Use TwelveData client

--- clients/src/main/scala/currexx/clients/Clients.scala
+++ clients/src/main/scala/currexx/clients/Clients.scala
@@ -8,6 +8,7 @@ import currexx.clients.broker.vindaloo.{VindalooClient, VindalooConfig}
 import currexx.clients.broker.xtb.{XtbClient, XtbConfig}
 import currexx.clients.data.MarketDataClient
 import currexx.clients.data.alphavantage.{AlphaVantageClient, AlphaVantageConfig}
+import currexx.clients.data.twelvedata.{TwelveDataClient, TwelveDataConfig}
 import org.typelevel.log4cats.Logger
 import sttp.capabilities.WebSockets
 import sttp.capabilities.fs2.Fs2Streams
@@ -15,6 +16,7 @@ import sttp.client3.SttpBackend
 
 final case class ClientsConfig(
     alphaVantage: AlphaVantageConfig,
+    twelveData: TwelveDataConfig,
     vindaloo: VindalooConfig,
     xtb: XtbConfig
 )
@@ -31,8 +33,9 @@ object Clients:
   ): F[Clients[F]] =
     for
       alphavantage <- AlphaVantageClient.make[F](config.alphaVantage, backend)
+      twelvedata   <- TwelveDataClient.make(config.twelveData, backend)
       vindaloo     <- VindalooClient.make[F](config.vindaloo, backend)
       xtb          <- XtbClient.make[F](config.xtb, backend)
       broker       <- BrokerClient.make[F](vindaloo, xtb)
-      data         <- MarketDataClient.make[F](alphavantage)
+      data         <- MarketDataClient.make[F](alphavantage, twelvedata)
     yield Clients[F](data, broker)
--- clients/src/main/scala/currexx/clients/data/MarketDataClient.scala
+++ clients/src/main/scala/currexx/clients/data/MarketDataClient.scala
@@ -3,6 +3,7 @@ package currexx.clients.data
 import cats.Monad
 import currexx.clients.HttpClient
 import currexx.clients.data.alphavantage.AlphaVantageClient
+import currexx.clients.data.twelvedata.TwelveDataClient
 import currexx.domain.market.{CurrencyPair, Interval, MarketTimeSeriesData, PriceRange}
 
 trait MarketDataClient[F[_]]:
@@ -10,14 +11,18 @@ trait MarketDataClient[F[_]]:
   def latestPrice(currencyPair: CurrencyPair): F[PriceRange]
 
 final private class LiveMarketDataClient[F[_]](
-    private val alphaVantageClient: AlphaVantageClient[F]
+    private val alphaVantageClient: AlphaVantageClient[F],
+    private val twelveDataClient: TwelveDataClient[F],
 ) extends MarketDataClient[F]:
   def latestPrice(currencyPair: CurrencyPair): F[PriceRange] =
-    alphaVantageClient.latestPrice(currencyPair)
+    twelveDataClient.latestPrice(currencyPair)
 
   def timeSeriesData(currencyPair: CurrencyPair, interval: Interval): F[MarketTimeSeriesData] =
-    alphaVantageClient.timeSeriesData(currencyPair, interval)
+    twelveDataClient.timeSeriesData(currencyPair, interval)
 
 object MarketDataClient:
-  def make[F[_]: Monad](alphaVantageClient: AlphaVantageClient[F]): F[MarketDataClient[F]] =
-    Monad[F].pure(LiveMarketDataClient[F](alphaVantageClient))
+  def make[F[_]: Monad](
+      alphaVantageClient: AlphaVantageClient[F],
+      twelveDataClient: TwelveDataClient[F]
+  ): F[MarketDataClient[F]] =
+    Monad[F].pure(LiveMarketDataClient[F](alphaVantageClient, twelveDataClient))
