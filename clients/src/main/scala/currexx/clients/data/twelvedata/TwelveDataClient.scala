0e18ed5ca56c730db21151cd4e64ead4a8b524eb
kirill5k <immotional@aol.com>
2022-11-09 12:56:09 +0000

    Tests for TwelveDataClient

--- clients/src/main/scala/currexx/clients/data/twelvedata/TwelveDataClient.scala
+++ clients/src/main/scala/currexx/clients/data/twelvedata/TwelveDataClient.scala
@@ -17,6 +17,7 @@ import sttp.client3.circe.asJson
 import sttp.client3.*
 import sttp.model.StatusCode
 
+import java.time.temporal.ChronoUnit
 import java.time.{Instant, LocalDate, LocalTime, ZoneOffset}
 import scala.concurrent.duration.*
 
@@ -44,32 +45,39 @@ final private class LiveTwelveDataClient[F[_]](
     fetchTimeSeriesData(pair, Interval.M1, 1).map(_.prices.head)
 
   private def fetchTimeSeriesData(pair: CurrencyPair, interval: Interval, numOfTicks: Int): F[MarketTimeSeriesData] =
-    val sym = s"${pair.base}/${pair.quote}"
-    val int = s"${interval.number}/${if (interval.unit == "hour") "h" else interval.unit.slice(0, 3)}"
-    val uri = uri"${config.baseUri}/time_series?symbol=$sym&interval=$int&apikey=${config.apiKey}&outputsize=$numOfTicks"
-    dispatch(basicRequest.get(uri).response(asJson[TimeSeriesResponse]))
-      .flatMap { r =>
-        r.body match
-          case Right(res) =>
-            val prices = res.values.zipWithIndex.map((v, i) => PriceRange(v.open, v.high, v.low, v.close, 0d, v.datetime.toInstant(i)))
-            MarketTimeSeriesData(pair, interval, prices).pure[F]
-          case Left(DeserializationException(responseBody, error)) =>
-            if (responseBody.contains("\"code\": 429"))
-              F.sleep(1.minute) >> fetchTimeSeriesData(pair, interval, numOfTicks)
-            else
-              logger.error(s"$name-client/json-parsing: ${error.getMessage}\n$responseBody") >>
-                F.raiseError(AppError.JsonParsingFailure(responseBody, s"Failed to parse $name response: ${error.getMessage}"))
-          case Left(HttpError(responseBody, StatusCode.Forbidden)) =>
-            logger.error(s"$name-client/forbidden\n$responseBody") >>
-              F.raiseError(AppError.AccessDenied(s"$name authentication has expired"))
-          case Left(HttpError(responseBody, status)) =>
-            logger.error(s"$name-client/${status.code}\n$responseBody") >>
-              F.sleep(delayBetweenConnectionFailures) >> fetchTimeSeriesData(pair, interval, numOfTicks)
+    cache
+      .get(pair -> interval)
+      .flatMap {
+        case Some(data) => data.pure[F]
+        case None =>
+          val sym = s"${pair.base}/${pair.quote}"
+          val int = s"${interval.number}${if (interval.unit == "hour") "h" else interval.unit.slice(0, 3)}"
+          val uri = uri"${config.baseUri}/time_series?symbol=$sym&interval=$int&apikey=${config.apiKey}&outputsize=$numOfTicks"
+          dispatch(basicRequest.get(uri).response(asJson[TimeSeriesResponse]))
+            .flatMap { r =>
+              r.body match
+                case Right(res) =>
+                  val prices = res.values.zipWithIndex.map((v, i) => PriceRange(v.open, v.high, v.low, v.close, 0d, v.datetime.toInstant(i)))
+                  MarketTimeSeriesData(pair, interval, prices).pure[F]
+                case Left(DeserializationException(responseBody, error)) =>
+                  if (responseBody.contains("\"code\": 429"))
+                    F.sleep(delayBetweenClientFailures) >> fetchTimeSeriesData(pair, interval, numOfTicks)
+                  else
+                    logger.error(s"$name-client/json-parsing: ${error.getMessage}\n$responseBody") >>
+                      F.raiseError(AppError.JsonParsingFailure(responseBody, s"Failed to parse $name response: ${error.getMessage}"))
+                case Left(HttpError(responseBody, StatusCode.Forbidden)) =>
+                  logger.error(s"$name-client/forbidden\n$responseBody") >>
+                    F.raiseError(AppError.AccessDenied(s"$name authentication has expired"))
+                case Left(HttpError(responseBody, status)) =>
+                  logger.error(s"$name-client/${status.code}\n$responseBody") >>
+                    F.sleep(delayBetweenConnectionFailures) >> fetchTimeSeriesData(pair, interval, numOfTicks)
+            }
+          .flatTap(data => cache.put(pair -> interval, data))
       }
 
   extension (ld: LocalDate)
     def toInstant(i: Int): Instant =
-      if (i == 0) ld.atTime(LocalTime.now()).toInstant(ZoneOffset.UTC) else ld.atStartOfDay().toInstant(ZoneOffset.UTC)
+      if (i == 0) ld.atTime(LocalTime.now().truncatedTo(ChronoUnit.MINUTES)).toInstant(ZoneOffset.UTC) else ld.atStartOfDay().toInstant(ZoneOffset.UTC)
 }
 
 private[clients] object TwelveDataClient {
@@ -91,4 +99,13 @@ private[clients] object TwelveDataClient {
       meta: TimeSeriesMeta,
       values: NonEmptyList[TimeSeriesValue]
   ) derives Codec.AsObject
+
+  def make[F[_]: Temporal: Logger](
+      config: TwelveDataConfig,
+      backend: SttpBackend[F, Any],
+      delayBetweenClientFailures: FiniteDuration = 1.minute
+  ): F[TwelveDataClient[F]] =
+    Cache
+      .make[F, (CurrencyPair, Interval), MarketTimeSeriesData](3.minutes, 15.seconds)
+      .map(cache => LiveTwelveDataClient(config, backend, delayBetweenClientFailures, cache))
 }
---
+++ clients/src/test/scala/currexx/clients/data/twelvedata/TwelveDataClientSpec.scala
@@ -0,0 +1,73 @@
+package currexx.clients.data.twelvedata
+
+import cats.effect.IO
+import currexx.clients.ClientSpec
+import currexx.domain.market.Currency.{EUR, USD}
+import currexx.domain.market.{CurrencyPair, Interval, PriceRange}
+import org.typelevel.log4cats.Logger
+import org.typelevel.log4cats.slf4j.Slf4jLogger
+import sttp.client3.{Response, SttpBackend}
+import sttp.model.StatusCode
+
+import java.time.{Instant, LocalTime}
+import scala.concurrent.duration.*
+
+class TwelveDataClientSpec extends ClientSpec {
+
+  given logger: Logger[IO] = Slf4jLogger.getLogger[IO]
+
+  val config = TwelveDataConfig("http://twelve-data.com", "api-key")
+  val pair   = CurrencyPair(EUR, USD)
+
+  "A TwelveDataClient" should {
+    "retrieve time-series data" in {
+      val requestParams = Map(
+        "symbol"     -> "EUR/USD",
+        "interval"   -> "1day",
+        "apikey"     -> "api-key",
+        "outputsize" -> "150"
+      )
+
+      val testingBackend: SttpBackend[IO, Any] = backendStub
+        .whenRequestMatchesPartial {
+          case r if r.isGet && r.isGoingTo("twelve-data.com/time_series") && r.hasParams(requestParams) =>
+            Response.ok(json("twelvedata/eur-usd-daily-prices.response.json"))
+          case _ => throw new RuntimeException()
+        }
+      val result = for
+        client <- TwelveDataClient.make[IO](config, testingBackend, 100.millis)
+        res    <- client.timeSeriesData(pair, Interval.D1)
+      yield res
+
+      result.asserting { timeSeriesData =>
+        val currentTime = LocalTime.now().toString.slice(0, 5)
+        timeSeriesData.currencyPair mustBe pair
+        timeSeriesData.interval mustBe Interval.D1
+        timeSeriesData.prices must have size 150
+        timeSeriesData.prices.head mustBe PriceRange(1.0027, 1.0032, 0.9973, 1.0005, 0.0, Instant.parse(s"2022-11-08T${currentTime}:00Z"))
+        timeSeriesData.prices.last mustBe PriceRange(1.05475, 1.055, 1.05405, 1.0547, 0.0, Instant.parse("2022-05-07T00:00:00Z"))
+      }
+    }
+
+    "retry after some delay in case of api limit error" in {
+      val testingBackend: SttpBackend[IO, Any] = backendStub.whenAnyRequest
+        .thenRespondCyclicResponses(
+          Response.ok(json("twelvedata/limit-error.json")),
+          Response.ok(json("twelvedata/eur-usd-daily-prices.response.json"))
+        )
+      val result = for
+        client <- TwelveDataClient.make[IO](config, testingBackend, 100.millis)
+        res    <- client.timeSeriesData(pair, Interval.D1)
+      yield res
+
+      result.asserting { timeSeriesData =>
+        val currentTime = LocalTime.now().toString.slice(0, 5)
+        timeSeriesData.currencyPair mustBe pair
+        timeSeriesData.interval mustBe Interval.D1
+        timeSeriesData.prices must have size 150
+        timeSeriesData.prices.head mustBe PriceRange(1.0027, 1.0032, 0.9973, 1.0005, 0.0, Instant.parse(s"2022-11-08T${currentTime}:00Z"))
+        timeSeriesData.prices.last mustBe PriceRange(1.05475, 1.055, 1.05405, 1.0547, 0.0, Instant.parse("2022-05-07T00:00:00Z"))
+      }
+    }
+  }
+}
