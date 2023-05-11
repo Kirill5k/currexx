e6d764e45591aab00388aaf07154f1fc65784563
kirill5k <immotional@aol.com>
2023-05-11 11:30:28 +0100

    Update time syntax

--- domain/src/main/scala/currexx/domain/time/syntax.scala
+++ domain/src/main/scala/currexx/domain/time/syntax.scala
@@ -15,27 +15,31 @@ object syntax {
 
   extension (ld: LocalDate) def toInstantAtStartOfDay: Instant = ld.atStartOfDay().toInstant(ZoneOffset.UTC)
   extension (ts: Instant)
-    def hour: Int              = ts.atZone(ZoneOffset.UTC).getHour
-    def dayOfWeek: DayOfWeek   = ts.atZone(ZoneOffset.UTC).getDayOfWeek
-    def toLocalDate: LocalDate = LocalDate.parse(ts.toString.slice(0, 10))
-    def atStartOfDay: Instant  = toLocalDate.atStartOfDay().toInstant(ZoneOffset.UTC)
-    def atEndOfDay: Instant    = toLocalDate.plusDays(1).atStartOfDay().minusSeconds(1).toInstant(ZoneOffset.UTC)
-    def durationBetween(otherTs: Instant): FiniteDuration =
-      math.abs(otherTs.toEpochMilli - ts.toEpochMilli).millis
-    def hasSameDateAs(otherTs: Instant): Boolean =
-      ts.toString.slice(0, 10) == otherTs.toString.slice(0, 10)
+    def minus(duration: FiniteDuration): Instant          = ts.minusNanos(duration.toNanos)
+    def plus(duration: FiniteDuration): Instant           = ts.plusNanos(duration.toNanos)
+    def hour: Int                                         = ts.atZone(ZoneOffset.UTC).getHour
+    def dayOfWeek: DayOfWeek                              = ts.atZone(ZoneOffset.UTC).getDayOfWeek
+    def toLocalDate: LocalDate                            = LocalDate.parse(ts.toString.slice(0, 10))
+    def atStartOfDay: Instant                             = toLocalDate.atStartOfDay().toInstant(ZoneOffset.UTC)
+    def atEndOfDay: Instant                               = toLocalDate.plusDays(1).atStartOfDay().minusSeconds(1).toInstant(ZoneOffset.UTC)
+    def durationBetween(otherTs: Instant): FiniteDuration = math.abs(otherTs.toEpochMilli - ts.toEpochMilli).millis
+    def hasSameDateAs(otherTs: Instant): Boolean          = ts.toString.slice(0, 10) == otherTs.toString.slice(0, 10)
 
   extension (fd: FiniteDuration)
-    def toReadableString: String = {
-      val hours   = fd.toHours
-      val remMins = fd - hours.hours
-      val minutes = remMins.toMinutes
-      val remSecs = remMins - minutes.minutes
-      val seconds = remSecs.toSeconds
-      s"""
-         |${if hours > 0 then s"${hours}h" else ""}
-         |${if minutes > 0 then s"${minutes}m" else ""}
-         |${if seconds > 0 then s"${seconds}s" else ""}
-         |""".stripMargin.replaceAll("\n", "")
-    }
+    def toReadableString: String =
+      val days     = fd.toDays
+      val remHours = fd - days.days
+      val hours    = remHours.toHours
+      val remMins  = remHours - hours.hours
+      val minutes  = remMins.toMinutes
+      val remSecs  = remMins - minutes.minutes
+      val seconds  = remSecs.toSeconds
+      val result =
+        s"""
+           |${if days > 0 then s"${days}d" else ""}
+           |${if hours > 0 then s"${hours}h" else ""}
+           |${if minutes > 0 then s"${minutes}m" else ""}
+           |${if seconds > 0 then s"${seconds}s" else ""}
+           |""".stripMargin.strip.replaceAll("\n", "")
+      if result == "" then "0s" else result
 }
