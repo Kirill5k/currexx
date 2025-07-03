4d17a287c7f7b1eb6dd947a87f3beb3defeff145
kirill5k <immotional@aol.com>
2025-07-03 19:21:19 +0100

    Setup spec for MarketProfile

---
+++ modules/core/src/test/scala/currexx/core/market/MarketProfileSpec.scala
@@ -0,0 +1,13 @@
+package currexx.core.market
+
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class MarketProfileSpec extends AnyWordSpec with Matchers {
+
+  "A MarketProfile" when {
+    "update" should {
+      
+    }
+  }
+}
