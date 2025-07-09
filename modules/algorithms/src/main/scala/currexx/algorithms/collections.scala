c2fdd9d3fe411157e92343d0cdefb9aae5c74aa0
kirill5k <immotional@aol.com>
2025-07-09 10:49:06 +0100

    Optimise pairs

--- modules/algorithms/src/main/scala/currexx/algorithms/collections.scala
+++ modules/algorithms/src/main/scala/currexx/algorithms/collections.scala
@@ -3,4 +3,13 @@ package currexx.algorithms
 private[algorithms] object collections:
   extension [A](seq: Vector[A])
     def pairs: Vector[(A, A)] =
-      seq.zip(seq.tail).zipWithIndex.collect { case (x, i) if i % 2 == 0 => x }
+      if (seq.length < 2) Vector.empty
+      else {
+        val result = Vector.newBuilder[(A, A)]
+        var i = 0
+        while (i < seq.length - 1) {
+          result += ((seq(i), seq(i + 1)))
+          i += 2
+        }
+        result.result()
+      }
