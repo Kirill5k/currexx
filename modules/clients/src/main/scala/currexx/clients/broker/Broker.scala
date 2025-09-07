dab86010aa20752ccdc82cdc73c3f10d4c0ef493
kirill5k <immotional@aol.com>
2025-09-07 10:19:12 +0100

    Create session with IG

--- modules/clients/src/main/scala/currexx/clients/broker/Broker.scala
+++ modules/clients/src/main/scala/currexx/clients/broker/Broker.scala
@@ -1,19 +1,20 @@
 package currexx.clients.broker
 
+import currexx.domain.market.Currency
 import org.latestbit.circe.adt.codec.*
 
 enum Broker:
   case Xtb, Ig
 
 enum BrokerParameters(val broker: Broker) derives JsonTaggedAdt.EncoderWithConfig, JsonTaggedAdt.DecoderWithConfig:
-  case Xtb(userId: String, password: String, demo: Boolean)                  extends BrokerParameters(Broker.Xtb)
-  case Ig(apiKey: String, username: String, password: String, demo: Boolean) extends BrokerParameters(Broker.Ig)
+  case Xtb(userId: String, password: String, demo: Boolean)                                      extends BrokerParameters(Broker.Xtb)
+  case Ig(apiKey: String, username: String, password: String, demo: Boolean, currency: Currency) extends BrokerParameters(Broker.Ig)
 
 object BrokerParameters:
   given JsonTaggedAdt.Config[BrokerParameters] = JsonTaggedAdt.Config.Values[BrokerParameters](
     mappings = Map(
       Broker.Xtb.toString.toLowerCase -> JsonTaggedAdt.tagged[BrokerParameters.Xtb],
-      Broker.Ig.toString.toLowerCase -> JsonTaggedAdt.tagged[BrokerParameters.Ig],
+      Broker.Ig.toString.toLowerCase  -> JsonTaggedAdt.tagged[BrokerParameters.Ig]
     ),
     strict = true,
     typeFieldName = "broker"
--- modules/clients/src/main/scala/currexx/clients/broker/ig/IgClient.scala
+++ modules/clients/src/main/scala/currexx/clients/broker/ig/IgClient.scala
@@ -2,11 +2,16 @@ package currexx.clients.broker.ig
 
 import cats.data.NonEmptyList
 import cats.effect.Async
+import cats.syntax.flatMap.*
+import io.circe.Codec
 import currexx.clients.Fs2HttpClient
 import currexx.clients.broker.BrokerParameters
+import currexx.domain.errors.AppError
 import currexx.domain.market.{CurrencyPair, OpenedTradeOrder, TradeOrder}
 import org.typelevel.log4cats.Logger
 import sttp.capabilities.fs2.Fs2Streams
+import sttp.client4.*
+import sttp.client4.circe.asJson
 import sttp.client4.WebSocketStreamBackend
 
 private[clients] trait IgClient[F[_]] extends Fs2HttpClient[F]:
@@ -26,4 +31,44 @@ final private class LiveXtbClient[F[_]](
   override def submit(params: BrokerParameters.Ig, order: TradeOrder): F[Unit] = ???
 
   override def getCurrentOrders(params: BrokerParameters.Ig, cps: NonEmptyList[CurrencyPair]): F[List[OpenedTradeOrder]] = ???
+
+  private def baseUrl(demo: Boolean): String =
+    val base = if demo then config.baseUri.replaceAll("api", "demo-api") else config.baseUri
+    s"${base}/gateway/deal"
+
+  private def login(params: BrokerParameters.Ig): F[IgClient.LoginResponse] =
+    dispatch(
+      basicRequest
+        .header("Version", "3")
+        .header("X-IG-API-KEY", params.apiKey)
+        .body(asJson(IgClient.LoginRequest(params.username, params.password)))
+        .response(asJson[IgClient.LoginResponse])
+        .post(uri"${baseUrl(params.demo)}")
+    ).flatMap {
+      case Right(res) =>
+        F.pure(res)
+      case Left(ResponseException.DeserializationException(responseBody, error, _)) =>
+        logger.error(s"$name-client/json-parsing: ${error.getMessage}\n$responseBody") >>
+          F.raiseError(AppError.JsonParsingFailure(responseBody, s"${name} client returned $error"))
+      case Left(ResponseException.UnexpectedStatusCode(body, meta)) =>
+        logger.error(s"$name-client/${meta.code.code}\n$body") >>
+          F.raiseError(AppError.ClientFailure(name, s"$name return ${meta.code}"))
+    }
+}
+
+object IgClient {
+
+  final case class LoginRequest(
+      identifier: String,
+      password: String
+  )
+
+  final case class LoginResponse(
+      accountId: String,
+      oauthToken: OauthToken
+  ) derives Codec.AsObject
+
+  final case class OauthToken(
+      access_token: String
+  )
 }
